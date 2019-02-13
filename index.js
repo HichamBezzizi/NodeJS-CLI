7f60ecddddca49cfb6271a049801fa86bb288555
Hicham Bezzizi <hicham_bezzizi@hotmail.com>
2019-02-13 16:11:43 +0100

    nodejs CLI done.

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 #!/usr/bin/env node
 
+//console.log("Hello, Node.JS!");
 
+const verifyEmail = require("email-validator");
+const getRequest = require("axios");
+const readLine = require("readline-sync");
 
+const email = readLine.question("What is your email? " )
 
+if(verifyEmail.validate(email) == true) {
 
+    const encoded = encodeURIComponent(email)
 
-console.log("Hello, Node.JS!");
+    getRequest.get("https://haveibeenpwned.com/api/v2/breachedaccount/"+encoded,{"headers" : {"user-agent": "Node_CLI tool"}})
+    .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
