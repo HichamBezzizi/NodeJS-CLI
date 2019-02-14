ad9385c2470f3e3dbf219e2728b9075e3d33898c
Hicham Bezzizi <hicham_bezzizi@hotmail.com>
2019-02-14 16:10:47 +0100

    removed some undesirable elements in my description using regexp.

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const getRequest = require("axios");            //
 const readLine = require("readline-sync");      //
 const chalk = require("chalk");                 //
 
-const validity = chalk.black.bgGreen.underline  // some styles for my confirmation messages.
-const errorMessage = chalk.black.bgRed.underline;//
+const validity = chalk.black.bgGreen; // some styles for my confirmation messages.
+const errorMessage = chalk.black.bgRed;//
 
 const inputEmail = process.argv[2];             //this makes sure you can enter the tool name and your email on 1 command line.
 //const inputEmail = readLine.question("Please enter your email here: " ) //makes sure this string is displayed in the terminal whenever you run your CLI tool.
@@ -23,23 +23,28 @@ if(verifyEmail.validate(inputEmail)) {
     getRequest.get(url,{"headers" : {"user-agent": "Node_CLI tool"}})
     .then(function (response) {
     //console.log(response.data);
-    console.log(errorMessage("This email has been breached!"))
+    console.log(errorMessage("::::::This email has been breached!::::::"))
     response.data.forEach(e => {
       name = e.Name
       date = e.BreachDate
       description = e.Description
-        
-      console.log(chalk.green("Location of the breach:"));
-      console.log(chalk.yellow(name));
-      console.log(chalk.green("Date of the breach:"));
-      console.log(chalk.yellow(date));
-      console.log(chalk.green("Reason for the breach:"));
-      console.log(chalk.yellow(description));
+      
+      newdesc = description.replace(/<\/?[^>]+(>|$)/g, ""); //makes sure the <a></a> tags are removed in the description
+      newdesc2 = newdesc.replace(/&quot;/g, '"');           //this makes sure the &quot; in the description gets removed and replace by actual quotation marks.
+
+
+
+      console.log(chalk.hex('#FF8800').bold("Location of the breach:"));
+      console.log(chalk.green(name));
+      console.log(chalk.hex('#FF8800').bold("Date of the breach:"));
+      console.log(chalk.green(date));
+      console.log(chalk.hex('#FF8800').bold("Reason for the breach:"));
+      console.log(chalk.green(newdesc2));
 })
   })
   .catch(function (error) {
     //console.log(error);
-    console.log(validity("This email has not been breached!"))
+    console.log(validity("::::::This email has not been breached!::::::"))
   });
 }
 else(console.log(errorMessage("Please enter a valid email.")))
